d639940fd4d775675e9333b1b8478f8f45899bd6
Amber Wang <amwang@microstrategy.com>
2021-07-08 13:43:20 -0400

    add search

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,23 +15,23 @@ class Navbar extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('a search value was submitted: ' + this.state.value);
+        window.open("https://community.microstrategy.com/s/global-search/%40uri?language=en_US#q=" + this.state.value)
         event.preventDefault();
     }
 
     render() {
         return (
             <div className="navbar">
-                <div className="navbar-container">
-                    <a href="https://www.microstrategy.com">
-                        <img src="ms-logo.png" alt="MicroStrategy Logo" name="navbar-logo" />
-                    </a>
-                    <form id="search" target="__blank">
-                    </form>
-                    <button onClick={function () { window.location = "https://www.microstrategy.com/en/support?now=expert" }} className="button">
-                        Talk to an expert
-                    </button>
-                </div>
+                <a href="https://www.microstrategy.com" id="logo">
+                    <img src="ms-logo.png" alt="MicroStrategy Logo" id="logo-img" />
+                </a>
+                <form onSubmit={this.handleSubmit} id="search">
+                    <input type='text' placeholder="Search MicroStrategy Community" value={this.state.value} onChange={this.handleChange} id="search-bar" />
+                    {/* <input type='submit' value="search" id="submit-button"/> */}
+                </form>
+                <button onClick={function () { window.location = "https://www.microstrategy.com/en/support?now=expert" }} id="talk-to-expert">
+                    Talk to an expert
+                </button>
             </div>
         );
     }
