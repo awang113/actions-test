fed8e617957d55ffb86fce5e81830a23cbd2ee1e
Amber Wang <amwang@microstrategy.com>
2021-07-14 14:14:25 -0400

    add course button

---
+++ src/buttondata.js
@@ -0,0 +1,88 @@
+export const sdk =
+    [
+        {
+            "name": "REST API",
+            "link": "https://github.microstrategy.com/pages/mshaffer/mstr-docs/",
+            "desc" : "A RESTful application designed to help developers build data-driven client applications quickly and easily.",
+            "color": "blue"
+        },
+        {
+            "name": "Embedding SDK",
+            "link": "https://lw.microstrategy.com/msdz/MSDL/GARelease_Current/docs/projects/EmbeddingSDK/default.htm",
+            "desc": "Quickly integrate a MicroStrategy dossier into a web application with functionality such as controlling navigation, retrieving, and applying files.",
+            "color": "orange"
+        },
+        {
+            "name": "Python SDK",
+            "link": "https://www2.microstrategy.com/producthelp/Current/mstrio-py/",
+            "desc": "not much text",
+            "color": "green"
+        },
+        {
+            "name": "Mobile SDK",
+            "color": "purple"
+        },
+        {
+            "name": "Visualization SDK",
+            "color": "pink"
+        },
+        {
+            "name": "Library SDK",
+            "color": "lightblue"
+        },
+        {
+            "name": "Data Connector SDK",
+            "color": "red"
+        },
+        {
+            "name": "Library Mobile SDK",
+            "color": "yellow"
+        },
+        {
+            "name": "Web SDK",
+            "color": "brown"
+        }
+    ]
+
+export const api = 
+[
+    {
+        "name": "Rest API",
+        "link": "https://demo.microstrategy.com/MicroStrategyLibrary/api-docs/#/",
+        "color": "blue"
+    },
+    {
+        "name": "Library SDK",
+        "color": "lightblue"
+    },
+    {
+        "name": "Visualization SDK",
+        "color": "pink"
+    },
+    {
+        "name": "Data Connector SDK",
+        "color": "red"
+    },
+    {
+        "name": "Mobile SDK",
+        "color": "purple"
+    },
+    {
+        "name": "Identity SDK",
+        "color": "gold"
+    }
+]
+
+export const courses =
+[
+    {
+        "name": "Pfizer Success Spotlight",
+        "desc": "January 17, 2021",
+        "link": "https://www.microstrategy.com/en/resources/events/world-2021/on-demand/pfizer-success-spotlight",
+        "src": "thumbnail1"
+    }
+]
+
+{/* <CourseButton name="Pfizer Success Spotlight" desc="January 17, 2021" link="https://www.microstrategy.com/en/resources/events/world-2021/on-demand/pfizer-success-spotlight" src={thumbnail1}/>
+          <CourseButton name="Embedding SDK" desc="March 2, 2015" link="https://www.microstrategy.com/en/business-intelligence/video/overview-of-the-microstrategy-embedded-analytics-platform" src={thumbnail2}/>
+          <CourseButton name="Embedding Dossiers onto Web Applications" desc="April 6, 2020" link="https://www.microstrategy.com/en/resources/events/world-2021/on-demand/embedding-dossiers-onto-web-applications" src={thumbnail3}/> */}
